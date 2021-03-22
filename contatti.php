e0cf4d755d35cd8384ad7e4a9128818f78d14109
Mr.Web <lombardi.nd@gmail.com>
2021-03-22 11:04:41 +0000

    wip

--- contatti.php
+++ contatti.php
@@ -15,55 +15,8 @@
         </div>
         </section><!-- End Breadcrumbs -->
         <section class="inner-page">
-          <div class="container" data-aos="fade-up">
-            <p>Studio Seniga offre servizi di consulenza fiscale, tributaria, amministrativa e contabile a società, ditte individuali e liberi professionisti.<br>
-              Il nostro obiettivo è di essere sempre efficenti grazie alla nostra professionalità, esperienza e competenza.<br>
-            Le frequenti modifiche delle leggi in materia fiscale rendono indispensabile un servizio di consulenza aggiornato e professionale che guidi le aziende, ed i liberi professionisti attraverso una corretta pratica fiscale.</p>
-            <p><strong>ATTIVITA’:<br>
-              </strong><br>
-              Offriamo consulenza su tutti gli aspetti legati alla creazione di un’impresa,<br>
-              assistiamo gli imprenditori in ambito societario, fiscale, civilistico e gestionale.<br>
-            In particolare:</p>
-            <ul>
-              <li>Analisi di fattibilità</li>
-              <li>Financial plan.</li>
-            </ul>
-            <p><strong>CONSULENZA DI LAVORO:</strong></p>
-            <ul>
-              <li>Gestione del personale;</li>
-              <li>Assistenza legale e amministrativa nei rapporti di lavoro.</li>
-            </ul>
-            <p><strong>CONSULENZA PREVIDENZIALE</strong></p>
-            <ul>
-              <li>Gestioni dei rapporti con gli Enti previdenziali ed assicurativi in materia di contribuzione e prestazioni ed in occasione di controlli, verifiche e di vigilanza in materia contributiva;</li>
-              <li>Consulenza nell’interno ambito della legislazione sociale.</li>
-            </ul>
-            <p><strong>CONSULENZA SINDACALE</strong></p>
-            <p>Relazioni industriali:</p>
-            <ul>
-              <li>Gestione dei rapporti con le OO.SS. interne ed esterne all’azienda;</li>
-              <li>Gestione e prevenzione del conflitto;</li>
-              <li>Assistenza in ipotesi di ricorso alla Cig, Cigs e licenziamento individuali e collettivi.</li>
-            </ul>
-            <p><strong>AMMINISTRAZIONE DEL PERSONALE</strong></p>
-            <ul>
-              <li>Elaborazione dei prospetti paga;</li>
-              <li>Calcolo delle contribuzioni ed imposte;</li>
-              <li>Elaborazione mensile, plurimensile ed annuale della modulistica necessaria per il versamento di ogni contribuzione obbligatoria e per le denunce agli enti ed istituti previdenziali ed assistenziali;</li>
-              <li>Gestione dei rapporti aziendali con istituti ed enti pubblici;</li>
-              <li>Determinazione dei trattamenti economici in caso di malattia, infortunio, maternità…ed espletamento dei relativi inadempimento nei confronti degli enti;</li>
-              <li>Elaborazione accantonamenti mensili e ratei retribuzione differita, tfr;</li>
-              <li>Elaborazione budget;</li>
-              <li>Scelta della tipologia societaria;</li>
-              <li>Analisi della struttura giuridica;</li>
-              <li>Redazione dello statuto sociale in applicazione delle norme generali e leggi speciali;</li>
-              <li>Costituzione società;</li>
-              <li>Apertura partita IVA;</li>
-              <li>Apertura posizioni previdenziali;</li>
-              <li>Apertura posizioni alla Camera di Commercio;</li>
-              <li>Analisi dei costi di gestione;</li>
-            </ul>
-          </p>
+          <div class="container">
+            <p>Lo Studio Seniga è raggiungibile tramite telefono allo <a href="tel:+390303759031">030 375 9031</a> oppure tramite la mail <a href="mailto:info@studioseniga.it">info@studioseniga.it</a></p>
         </div>
       </section>
       </main><!-- End #main -->
