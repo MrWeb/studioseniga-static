626548a1cb0704e704e89b80c7da4b36a99710e4
Mr.Web <lombardi.nd@gmail.com>
2022-04-03 11:59:31 +0100

    Rag to Dott

--- su-di-noi.php
+++ su-di-noi.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include_once 'html-header.php';?>
+<?php include_once 'html-header.php'; ?>
 
 <body>
 
-  <?php include_once 'header.php';?>
+  <?php include_once 'header.php'; ?>
 
   <main id="main">
 
@@ -25,7 +25,7 @@
     <section class="inner-page">
       <div class="container" data-aos="fade-up">
         <p>Lo Studio Seniga e Associati nasce dalla convinzione che ora, e sempre di più in futuro, le attività professionali potranno essere svolte con gli elevati standards qualitativi, richiesti da un mercato che si evolve con grandissima rapidità e solo da strutture formate da diversi professionisti.</p>
-        <p>La Rag. Francesca Seniga iscritta all’Ordine dei Commercialisti di Brescia al numero 771 in data 08/04/1992 nonchè all’albo dei Consulenti Tecnici del Tribunale di Brescia in data 14/10/1993 ed è iscritta all’albo dei revisori legali con decreto legislativo 27/01/1992. Esercita la propria attività in Brescia (BS) – Via Gerolamo Romanino n.1 e occupa una superficie di circa 200 mq.</p>
+        <p>La Dott.sa Francesca Seniga iscritta all’Ordine dei Commercialisti di Brescia al numero 771 in data 08/04/1992 nonchè all’albo dei Consulenti Tecnici del Tribunale di Brescia in data 14/10/1993 ed è iscritta all’albo dei revisori legali con decreto legislativo 27/01/1992. Esercita la propria attività in Brescia (BS) – Via Gerolamo Romanino n.1 e occupa una superficie di circa 200 mq.</p>
         <p>Si avvale di 3 collaboratrici.</p>
         </p>
       </div>
@@ -34,7 +34,7 @@
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-  <?php include_once 'footer.php';?>
+  <?php include_once 'footer.php'; ?>
   <!-- End Footer -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
